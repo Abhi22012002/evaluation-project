16d492a3ae7ebae19f253a92862a647ea0d1d6cd
Abhi22012002 <abhijeetshelar70@gmail.com>
2023-09-15 13:36:04 +0530

    rules button added

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let dispRules=function(){
+
+    document.getElementById("rules-model").style.display = "flex";
+}
+let hideRules=function(){
+    document.getElementById("rules-model").style.display = "none";
+}
