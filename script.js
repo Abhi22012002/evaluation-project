3f7946a3782b740dd627a809a4117f42aae82a08
Abhi22012002 <abhijeetshelar70@gmail.com>
2023-09-18 09:11:25 +0530

    Result board Design complete

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 
+
+
 let dispRules=function(){
 
     document.getElementById("rules-model").style.display = "flex";
 }
 let hideRules=function(){
     document.getElementById("rules-model").style.display = "none";
+}
+
+let show=function(){
+
+const playingarea=document.querySelector("#root-playingarea");
+const result=document.querySelector("#root-result");
+    
+    playingarea.style.display="none";
+    result.style.display="flex";
+
+    document.getElementById("you-picked").className= document.getElementById("rock").className;
+    document.getElementById("you-picked-image").src="rock.png";
+    document.getElementById("computer-picked").className= document.getElementById("paper").className;
+    document.getElementById("computer-picked-image").src="paper.png";
+
+
 }
